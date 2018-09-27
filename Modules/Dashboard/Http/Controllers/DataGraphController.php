cd9d96ca7d5aa9e01a5f5636bf121478521c9bdc
Guilherme Ferreira <gmferreir1@gmail.com>
2018-09-27 15:45:47 -0300

    Finalizando os graficos de contratos celebrados

--- Modules/Dashboard/Http/Controllers/DataGraphController.php
+++ Modules/Dashboard/Http/Controllers/DataGraphController.php
@@ -3,23 +3,34 @@
 namespace Modules\Dashboard\Http\Controllers;
 
 use App\Traits\Generic\DateTime;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Register\Services\ReserveContract\ReserveContractServiceCrud;
+use Modules\SystemGoal\Services\SystemGoalServiceCrud;
+use Modules\Termination\Services\ContractServiceCrud;
 
 class DataGraphController extends Controller
 {
     use DateTime;
 
     /**
+     * @var SystemGoalServiceCrud
+     */
+    private $systemGoalServiceCrud;
+    /**
      * @var ReserveContractServiceCrud
      */
-    private $reserveContractRepository;
+    private $reserveContractServiceCrud;
+    /**
+     * @var ContractServiceCrud
+     */
+    private $terminationContractServiceCrud;
 
-    public function __construct(ReserveContractServiceCrud $reserveContractRepository)
+    public function __construct(ReserveContractServiceCrud $reserveContractServiceCrud, SystemGoalServiceCrud $systemGoalServiceCrud, ContractServiceCrud $terminationContractServiceCrud)
     {
-        $this->reserveContractRepository = $reserveContractRepository;
+        $this->systemGoalServiceCrud = $systemGoalServiceCrud;
+        $this->reserveContractServiceCrud = $reserveContractServiceCrud;
+        $this->terminationContractServiceCrud = $terminationContractServiceCrud;
     }
 
     /**
@@ -52,10 +63,10 @@ class DataGraphController extends Controller
         };
 
 
-        $dataReserve = $this->reserveContractRepository->scopeQuery($closureReserve);
-        $dataAssigned = $this->reserveContractRepository->scopeQuery($closureAssigned);
-        $dataReserveCanceledMonth = $this->reserveContractRepository->scopeQuery($closureReserveCanceledMonth);
-        $dataCanceledMonth = $this->reserveContractRepository->scopeQuery($closureCanceledMonth);
+        $dataReserve = $this->reserveContractServiceCrud->scopeQuery($closureReserve);
+        $dataAssigned = $this->reserveContractServiceCrud->scopeQuery($closureAssigned);
+        $dataReserveCanceledMonth = $this->reserveContractServiceCrud->scopeQuery($closureReserveCanceledMonth);
+        $dataCanceledMonth = $this->reserveContractServiceCrud->scopeQuery($closureCanceledMonth);
 
 
         return [
@@ -109,6 +120,86 @@ class DataGraphController extends Controller
 
             ],
             'month' => getCurrentMonth()
+        ];
+    }
+
+
+    public function dataGraphContractCelebrated(Request $request)
+    {
+        $queryParams = [
+            'type_goal' => !$request->get('type_goal') ? 'value' : $request->get('type_goal')
+        ];
+
+        $month = date('m');
+        $year = date('Y');
+
+        $goal = 0;
+
+        // verifica se tem meta registrada no sistema
+        $checkGoal = $this->systemGoalServiceCrud->findWhere(['name' => 'cc']);
+        if (!$checkGoal->count()) {
+            return [
+                'error' => true,
+                'message' => 'no goals found'
+            ];
+        }
+
+
+        // verifica o tipo de meta solicitada
+        if ($queryParams['type_goal'] === 'value') {
+            // valor
+            $goal = $checkGoal[0]['value'];
+        } else {
+            // percentual
+            $getGoalPercent = $this->systemGoalServiceCrud->findWhere(['name' => 'cc', 'type' => 'percent']);
+
+            if (!$getGoalPercent->count()) {
+                return [
+                    'error' => true,
+                    'message' => 'no goal percent found'
+                ];
+            }
+
+            // pego o valor de inativados do mês
+            $closureContractsInactivatedMonth =  function ($query) use ($year, $month) {
+                return $query->where('type_register', 'termination')
+                            ->whereBetween('end_process', [$this->initDateCurrentMonth($year, $month), $this->endDateCurrentMonth($year, $month)])
+                            ->whereIn('status', array('r', 'a', 'j', 'cej'));
+            };
+            $contractInactivatedMonth = $this->terminationContractServiceCrud->scopeQuery($closureContractsInactivatedMonth);
+            $valueContractInactivated = $contractInactivatedMonth->sum('value');
+
+            $goal = (float) round(($valueContractInactivated * ($getGoalPercent[0]['percent'] / 100)) + $valueContractInactivated, 2);
+        }
+
+        // verifica a meta no sistema
+        $month = date('m');
+        $year = date('Y');
+
+        $closure = function ($query) use ($year, $month) {
+            return $query->whereIn('situation', array('as', 'ap', 'af'))
+                    ->whereBetween('conclusion', [$this->initDateCurrentMonth($year, $month), $this->endDateCurrentMonth($year, $month)]);
+        };
+
+        $getGoal = $this->reserveContractServiceCrud->scopeQuery($closure);
+
+
+
+        return [
+            'data' => [
+                [
+                    'name' => 'Meta Cumprida',
+                    'y' => $getGoal->sum('value_negotiated'),
+                    'color' => 'darkgreen'
+                ],
+                [
+                    'name' => 'Meta a Cumprir',
+                    'y' => $goal - $getGoal->sum('value_negotiated'),
+                    'color' => 'darkred'
+                ]
+            ],
+            'month' => getCurrentMonth(),
+            'goal' => (float) $goal
         ];
     }
 }
--- Modules/Dashboard/Http/routes.php
+++ Modules/Dashboard/Http/routes.php
@@ -12,6 +12,7 @@ Route::group(['middleware' => ['auth:api'], 'prefix' => 'dashboard', 'namespace'
      * Route::dashboard/data-graph
      */
     Route::group(['prefix' => 'data-graph'], function () {
+        Route::get('contracts-celebrated/{queryParams?}', 'DataGraphController@dataGraphContractCelebrated');
         Route::get('contracts-celebrated-per-status', 'DataGraphController@dataGraphContractCelebratedPerStatus');
     });
 
